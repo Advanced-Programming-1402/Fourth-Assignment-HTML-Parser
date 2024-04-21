b3e1c22551c749efee37f75a41ac1adba3a0e4b0
YasharNasiri <bluemustarduniversity@gmail.com>
2024-04-21 18:17:20 +0330

    Initial commit [someone will be murdered for how long it took to find a vpn to fucking download gradel]

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,28 +9,32 @@ import java.util.*;
 public class Parser {
     static List<Country> countries = new ArrayList<>();
 
-    public List<Country> sortByName(){
+    public List<Country> sortByName()
+    {
         List<Country> sortedByName = new ArrayList<>(countries);
         // Sort countries alphabetically (least)
         //TODO
         return  sortedByName;
     }
 
-    public List<Country> sortByPopulation(){
+    public List<Country> sortByPopulation()
+    {
         List<Country> sortedByPopulation = new ArrayList<>(countries);
         // Sort countries by population (most)
         //TODO
         return sortedByPopulation;
     }
 
-    public List<Country> sortByArea(){
+    public List<Country> sortByArea()
+    {
         List<Country> sortedByArea = new ArrayList<>(countries);
         // Sort countries by area (most)
         //TODO
         return sortedByArea;
     }
 
-    public void setUp() throws IOException {
+    public void setUp() throws IOException
+    {
 
         //Parse the HTML file using Jsoup
         //TODO
@@ -42,7 +46,9 @@ public class Parser {
         //TODO
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         //you can test your code here before you run the unit tests ;)
+        System.out.println("poopoo");
     }
 }
